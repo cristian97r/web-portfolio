3d51dd4c76010fa4c61676fd7668111c3b4c6b08
cristian97r <cristian97drb@gmail.com>
2019-02-13 00:38:30 -0400

    add express.js

---
+++ app.js
@@ -0,0 +1,10 @@
+var express    = require('express'),
+app            = express();
+
+
+app.get("/", function(req, res) {
+    res.render("index.html");  
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+});
