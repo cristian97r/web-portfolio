61c98e83777f419a657af44a7ea98d31740dc6fa
cristian97r <cristian97drb@gmail.com>
2019-02-13 12:41:15 -0400

    add carousel fade slides

--- app.js
+++ app.js
@@ -9,6 +9,6 @@ app.get("/", function(req, res) {
     res.render("index");  
 });
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(3000, process.env.IP, function(){
     console.log("the app is running")
 });
